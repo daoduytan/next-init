2f54706ac7bb8c6180cdd3ad0ff34c5a4336b891
daoduytan <ddtan2212@gmail.com>
2020-11-18 09:44:44 +0700

    add format code

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,17 @@
 // import { createServer } from "http";
-import express from "express";
-import next from "next";
-import { routes } from "./routes";
+import express from 'express';
+import next from 'next';
+import { routes } from './routes';
 
 const port = parseInt(process.env.PORT!, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   const server = express();
 
-  server.all("*", (req, res) => {
+  server.all('*', (req, res) => {
     return handle(req, res);
   });
 
--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
-import nextRoutes from "next-routes";
+import nextRoutes from 'next-routes';
 
 export const routes = new nextRoutes();
 export const Router = routes.Router;
 export const Link = routes.Link;
 
-routes.add("confirm", "/user/confirm/:token");
+routes.add('confirm', '/user/confirm/:token');

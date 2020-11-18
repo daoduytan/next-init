f01bcf3ca8f016bb64a1a51af5d5a03a1136507d
daoduytan <ddtan2212@gmail.com>
2020-11-18 11:15:53 +0700

    add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
+import { ReactQueryCacheProvider, QueryCache } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
 import '../styles/globals.css';
 
+const queryCache = new QueryCache();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </ReactQueryCacheProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
 import Head from 'next/head';
+import { useQuery } from 'react-query';
+import { QueryCache } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
 
+const fetchData = () => {
+  return {
+    data: 1,
+  };
+};
+
+export async function getStaticProps() {
+  const queryCache = new QueryCache();
+
+  await queryCache.prefetchQuery('data', fetchData);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryCache),
+    },
+  };
+}
 export default function Home() {
+  const { data, isFetching } = useQuery('data', fetchData);
+
+  console.log(data);
+  if (isFetching) return <div>Loading</div>;
+
   return (
     <div>
       <Head>
@@ -8,6 +33,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       Homepage
+      {data && data.data}
     </div>
   );
 }
